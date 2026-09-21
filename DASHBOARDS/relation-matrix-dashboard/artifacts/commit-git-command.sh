69f0842c17dbd9204c3fa675e8954f61f1e1028b
James Aaron <jasksg9@gmail.com>
2026-09-21 05:41:26 -0400

    Create commit-git-command.sh

---
+++ DASHBOARDS/relation-matrix-dashboard/artifacts/commit-git-command.sh
@@ -0,0 +1,11 @@
+git add \
+  relation-matrix-dashboard/README.md \
+  relation-matrix-dashboard/MATHEMATICAL_SPECIFICATION.md \
+  relation-matrix-dashboard/INPUT_OUTPUT_SCHEMA.md \
+  relation-matrix-dashboard/TESTING.md \
+  relation-matrix-dashboard/.gitignore \
+  relation-matrix-dashboard/examples \
+  relation-matrix-dashboard/fixtures \
+  relation-matrix-dashboard/artifacts/.gitkeep
+
+git commit -m "Document relation matrix Smith normal form dashboard"
