38eb1ba4e9f1fa7449aacf27c0af7f6d2ee9570b
James Aaron <jasksg9@gmail.com>
2026-09-21 20:46:50 -0400

    Add pullback_stable_join theorem for join relations

--- Aqarion-Lean/Pending/pullback_equivgen_subset.lean
+++ Aqarion-Lean/Pending/pullback_equivgen_subset.lean
@@ -7,3 +7,23 @@ theorem pullback_equivGen_subset {α : Type}
   | refl c => exact EquivGen.refl (T c)
   | symm _ ih => exact EquivGen.symm ih
   | trans _ _ ih1 ih2 => exact EquivGen.trans ih1 ih2
+
+---
+
+theorem pullback_stable_join {α : Type}
+    (T : α → α) (E F : EquivRel α)
+    (hE : PullbackStable T E)
+    (hF : PullbackStable T F) :
+    ∀ a b, joinRel E F (T a) (T b) → joinRel E F a b := by
+  intro a b h
+  induction h with
+  | of hab =>
+      exact EquivGen.of (Or.elim hab
+        (fun h => Or.inl (hE _ _ h))
+        (fun h => Or.inr (hF _ _ h)))
+  | refl c =>
+      exact EquivGen.refl c
+  | symm _ ih =>
+      exact EquivGen.symm ih
+  | trans _ _ ih1 ih2 =>
+      exact EquivGen.trans ih1 ih2
